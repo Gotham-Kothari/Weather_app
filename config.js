082d4c496508d39712da41f57098d71639faf335
Gotham Kothari <gothamkothari640@gmail.com>
2025-04-18 12:11:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const { contextBridge } = require('electron');
 const config = {
-    OPENWEATHER_API_KEY: '76039c6bc2064d2285fbf682467ce9d0',
+    OPENWEATHER_API_KEY: //Add your own API key here,
     BASE_URL: 'https://api.openweathermap.org/data/2.5/weather',
     DEFAULT_CITY: 'New York',
     UNITS: 'metric',
